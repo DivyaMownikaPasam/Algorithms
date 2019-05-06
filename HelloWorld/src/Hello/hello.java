95d4fc393b9b62595e5d57b763839952445be66d
Divya Mownika Pasam <divya.mownika@gmail.com>
2019-05-05 22:14:30 -0700

    Accepting inputs

--- HelloWorld/src/Hello/hello.java
+++ HelloWorld/src/Hello/hello.java
@@ -1,9 +1,19 @@
 package Hello;
 
+import java.util.Scanner;
+
 public class hello {
 
 	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); 
+				
+		// Read a full line of input from stdin and save it to our variable, inputString.
+		String inputString = scan.nextLine(); 
+		scan.close(); 
+		
 		System.out.println("Hello World!!");
+		System.out.println(inputString);
 
 	}
 

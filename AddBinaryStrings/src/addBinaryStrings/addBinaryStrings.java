00ee298b3baf3a3752810e1d07ff11c7d0fac844
Divya Mownika Pasam <divya.mownika@gmail.com>
2019-05-06 23:21:29 -0700

    no message

--- AddBinaryStrings/src/addBinaryStrings/addBinaryStrings.java
+++ AddBinaryStrings/src/addBinaryStrings/addBinaryStrings.java
@@ -3,8 +3,14 @@ package addBinaryStrings;
 public class addBinaryStrings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Adding two binary numbers");
+		
+		System.out.println("Adding two binary strings");
+		System.out.println(addBinary("1010","1011"));
+		
 	}
+	
+	public static String addBinary(String a, String b) {
+        return(Long.toBinaryString(Long.parseLong(a,2)+Long.parseLong(b,2)));
+    }
 
 }
